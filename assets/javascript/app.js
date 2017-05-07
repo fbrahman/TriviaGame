79c7a5d8406ab198032ebceeb5cd83340aaa8879
fbrahman <fbrahman@gmail.com>
2017-05-07 14:00:37 -0700

    timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,39 @@
 var quiz = {
-	correct:0,
-	totalQuestions:5,
-	time: 90,
-	
+    correct: 0,
+    totalQuestions: 5,
+    time: 90,
+    timevar: "",
 
-	displayQuestion: function(question){
 
-	},
+    displayQuestion: function(question) {
 
-	checkUserInput: function(question, userAnswer){
+    },
 
-	},
+    checkUserInput: function(question, userAnswer) {
 
-	calculateScore: function(correct, totalQuestions){
+    },
 
-	},
+    calculateScore: function(correct, totalQuestions) {
 
-	timer: function(time){
-		let timer = time;
-		console.log("this is the timer",timer)
+    },
 
-		setInterval(function(){
-			timer--;
-			return timer;
-		},1000);
-	}
+    timer: function(time) {
+        let timer = time;
+        timevar = setInterval(function() {
+            
+            timer--;
+           
+            $("#timer").html(timer);
+
+            if (timer === 0){
+            	quiz.stoptimer();
+            };
+        }, 1000);
+
+    },
+
+    stoptimer: function (){
+    	clearInterval(timevar);
+    },
+
 }
